a977affe34e23583161a3e22706d9dcb310aef5f
vidushig08 <60618677+vidushig08@users.noreply.github.com>
2020-11-19 17:39:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+const navSlide = () => {
+    const mobile = document.querySelector('.mobile');
+    const nav = document.querySelector('.navlinks');
+    const navLinks = document.querySelectorAll('.navlinks li');
+    
+    mobile.addEventListener('click',() => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation ='';
+            } else{
+                link.style.animation = `.navLinkFade 0.5s ease forwards ${index / 5}s`;
+            }            
+    });
+});
+} 
+
+navSlide();
+*/
