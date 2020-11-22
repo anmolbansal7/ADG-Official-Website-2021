ecad1c7dc8a8a932fa23a4a1eabae5e7d73e71df
Anmol Bansal <abanmolbansal5@gmail.com>
2020-11-22 13:56:31 +0530

    commit

---
+++ js/preloader.js
@@ -0,0 +1,7 @@
+var overlay = document.getElementById("overlay");
+var main = document.getElementById("main");
+window.addEventListener("load", function () {
+	overlay.style.display = "none";
+
+	main.style.display = "initial";
+});
