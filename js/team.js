692da18b9a65c45b2acdead5c7b0e6b3656c8d1f
anmolbansal7 <abanmolbansal5@gmail.com>
2020-12-11 20:31:02 +0530

    small fix

--- js/team.js
+++ js/team.js
@@ -10,16 +10,20 @@ toggleSwitch.addEventListener('click', function() {
 		toggleContainer.style.clipPath = 'inset(0 0 0 50%)';
 		toggleContainer.style.backgroundColor = '#D74046';
 		t1.style.display = "none";
+		//t1.style.visibility = "hidden";
 		// t1.style.clipPath = 'inset(0 0 0 50%)';
 		t2.style.display = "flex";
+		//t1.style.visibility = "visible";
 		// t2.style.clipPath = 'inset(0 50% 0 0)';
 		
 	} else {
 		toggleContainer.style.clipPath = 'inset(0 50% 0 0)';
 		toggleContainer.style.backgroundColor = 'dodgerblue';
 		t1.style.display = "flex";
+		//t1.style.visibility = "visible";
 		// t1.style.clipPath = 'inset(0 50% 0 0)';
 		t2.style.display = "none";
+		//t1.style.visibility = "hidden";
 		// t2.style.clipPath = 'inset(0 0 0 50%)';
 	}
 	console.log(toggleNumber)
