3330bf642e9eae16ef2798ae35ed4fb7c50941e9
anmolbansal7 <abanmolbansal5@gmail.com>
2020-12-15 19:53:26 +0530

    contact layout complete

--- js/foot-script.js
+++ js/foot-script.js
@@ -5,6 +5,7 @@ const body = document.body;
 const nav = document.getElementById("navbar");
 const foot = document.getElementById("footer");
 
+
 //Partners Page
 const lightImg = document.getElementById("light-mode");
 const darkImg = document.getElementById("dark-mode");
@@ -21,6 +22,15 @@ const darkImg4 = document.getElementById("dark-mode4");
 const abtImgd = document.getElementById("dark1");
 const abtImgl = document.getElementById("light1");
 
+//Contact Page
+const contactBox = document.getElementById("contact-form");
+const nameY = document.getElementById("name");
+const mail = document.getElementById("email");
+const phone = document.getElementById("phone");
+const msg = document.getElementById("message");
+const send = document.getElementById("send");
+const contactInfo = document.getElementById("contact-info");
+const moreInfo = document.getElementById("more-info");
 
 toggle.addEventListener("input", (e) => {
 	const isChecked = e.target.checked;
@@ -29,6 +39,17 @@ toggle.addEventListener("input", (e) => {
 		body.classList.add("light-theme");
 		nav.classList.add("light-theme");
 		foot.classList.add("light-theme");
+		
+		//Contact Page
+		contactBox.classList.add("light-theme");
+		nameY.classList.add("light-theme");
+		mail.classList.add("light-theme");
+		phone.classList.add("light-theme");
+		msg.classList.add("light-theme");
+		send.classList.add("light-theme");
+		contactInfo.classList.add("light-theme");
+		moreInfo.classList.add("light-theme");
+
 		lightImg.style.display = "inline";
 		darkImg.style.display = "none";
 		lightImg1.style.display = "inline";
@@ -46,6 +67,17 @@ toggle.addEventListener("input", (e) => {
 		body.classList.remove("light-theme");
 		nav.classList.remove("light-theme");
 		foot.classList.remove("light-theme");
+
+		//Contact Page
+		contactBox.classList.remove("light-theme");
+		nameY.classList.remove("light-theme");
+		mail.classList.remove("light-theme");
+		phone.classList.remove("light-theme");
+		msg.classList.remove("light-theme");
+		send.classList.remove("light-theme");
+		contactInfo.classList.remove("light-theme");
+		moreInfo.classList.remove("light-theme");
+
 		lightImg.style.display = "none";
 		darkImg.style.display = "inline";
 		lightImg1.style.display = "none";
@@ -64,49 +96,49 @@ toggle.addEventListener("input", (e) => {
 
 //Persisting User's Choice
 
-const btn = document.getElementById("toggle");
+// const btn = document.getElementById("toggle");
 
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "light") {
-  body.classList.add("light-theme");
-}
+// const currentTheme = localStorage.getItem("theme");
+// if (currentTheme == "light") {
+//   body.classList.add("light-theme");
+// }
 
-btn.addEventListener("click", function () {
-  body.classList.toggle("light-theme");
+// btn.addEventListener("click", function () {
+//   body.classList.toggle("light-theme");
 
-  let theme = "dark";
-  if (document.body.classList.contains("light-theme")) {
-    theme = "light";
-  }
-  localStorage.setItem("theme", theme);
-});
+//   let theme = "dark";
+//   if (document.body.classList.contains("light-theme")) {
+//     theme = "light";
+//   }
+//   localStorage.setItem("theme", theme);
+// });
 
 
 
-function App() {}
+// function App() {}
 
-App.prototype.setState = function(state) {
-  localStorage.setItem('checked', state);
-}
+// App.prototype.setState = function(state) {
+//   localStorage.setItem('checked', state);
+// }
 
-App.prototype.getState = function() {
-  return localStorage.getItem('checked');
-}
+// App.prototype.getState = function() {
+//   return localStorage.getItem('checked');
+// }
 
-function init() {
-  var app = new App();
-  var state = app.getState();
-  var checkbox = document.querySelector('#toggle');
+// function init() {
+//   var app = new App();
+//   var state = app.getState();
+//   var checkbox = document.querySelector('#toggle');
 
-  if (state == 'true') {
-    checkbox.checked = true;
-  }
+//   if (state == 'true') {
+//     checkbox.checked = true;
+//   }
 
-  checkbox.addEventListener('click', function() {
-      app.setState(checkbox.checked);
-  });
-}
+//   checkbox.addEventListener('click', function() {
+//       app.setState(checkbox.checked);
+//   });
+// }
 
-init();
+// init();
 
 
