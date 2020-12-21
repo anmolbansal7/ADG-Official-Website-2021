49a94b56a2a96bf4e24cd8886bd7fdcde092a02d
anmolbansal7 <abanmolbansal5@gmail.com>
2020-12-21 19:42:48 +0530

    update

--- js/home-anim.js
+++ js/home-anim.js
@@ -58,16 +58,9 @@ var introVid = document.getElementById('introVid');
 
     .on("leave", function(){
         introVid.pause();
-    })  
-    
-    
-    //refresh page on browser resize
-$(window).bind('resize', function(e)
-{
-  console.log('window resized..');
-  this.location.reload(false); /* false to get page from cache */
-  /* true to fetch page from server */
-});
+    }) 
+     
+    window.onresize = function(){ location.reload(); }
 
 } else if (width <= 1024) {
         //pin the girl
